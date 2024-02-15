e44331c1a1acbfaf9da7a48f7e4dd21212245a76
KhanLee0930 <e1143336@u.nus.edu>
2024-02-15 17:06:05 +0800

    2.15

--- test/test.py
+++ test/test.py
@@ -1,10 +1,14 @@
-from data_loader import get_train_datalist
+from tqdm import tqdm
 
+from data_loader_test import get_train_datalist
+from data_loader_test import get_dataloader
+import argparse
+import pandas as pd
 
 def datalist_test():
     parser = argparse.ArgumentParser(description='Example of parser. ')
         # Data root.
-    parser.add_argument("--data_root", type=str, default='./')
+    parser.add_argument("--data_root", type=str, default='/root/SED_Adaptation_Classifier/Datasets/ESC-50-master/collection')
     parser.add_argument('--exp_name', type=str, default='disjoint')
     parser.add_argument('--workspace', type=str, default='workspace')
     parser.add_argument('--batch_size', type=int, default=128)
@@ -21,14 +25,14 @@ def datalist_test():
         help="memory management [random, uncertainty, reservoir, prototype]",
     )
     parser.add_argument("--num_pretrain_class", type=int, default=None, help="The number of classes in pretrained model")
-    parser.add_argument("--n_tasks", type=int, default=5, help="The number of tasks")
+    parser.add_argument("--n_tasks", type=int, default=1, help="The number of tasks")
     parser.add_argument(
-        "--n_cls_a_task", type=int, default=2, help="The number of class of each task"
+        "--n_cls_a_task", type=int, default=50, help="The number of class of each task"
     )
     parser.add_argument(
         "--n_init_cls",
         type=int,
-        default=2,
+        default=50,
         help="The number of classes of initial task",
     )
     parser.add_argument("--rnd_seed", type=int, default=5, help="Random seed number.")
@@ -51,4 +55,14 @@ def datalist_test():
     parser.add_argument("--debug", action="store_true", help="Turn on Debug mode")
     args = parser.parse_args()
     print(args)
-    # get_train_datalist(args,0)
+    # type(pd.read_json('/root/SED_Adaptation_Classifier/Datasets/ESC-50-master/collection/ESC-50_train_disjoint_rand5_cls50_task0_1.json'))
+    train_list = get_train_datalist(args,0)[0]
+
+    train_loader = get_dataloader(pd.DataFrame(train_list), 'ESC-50', split='train', batch_size=128, num_class=50,
+                                      num_workers=8)
+    print(train_loader)
+    for batch_data_dict in train_loader:
+        print(batch_data_dict)
+
+if __name__ == "__main__":
+    datalist_test()
